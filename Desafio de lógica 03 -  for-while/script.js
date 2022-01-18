a061648a63741a36176571dfd5122b9c6a640058
Daniel Pontes <dancpontes@protonmail.com>
2022-01-18 17:55:22 -0300

    Desafio 03 completo

--- Desafio de lógica 03 -  for-while/script.js
+++ Desafio de lógica 03 -  for-while/script.js
@@ -1,10 +1,23 @@
 //1 - Criar um loop que conte de 1 até 10 usando FOR
+for(let a = 1; a <= 10; a++){
+    console.log(a)
+}
 
 
 //2 - Criar um loop que conte de 10 até 1 usando WHILE
-
+let b = 10
+while(1 <= b){
+    console.log(b)
+    b --
+}
 
 //3 - Criar um loop que conte todos os números ímpares de 1 até 100
+for(let c = 1; c <= 100; c+=2){
+    console.log(c)
+}
 
 
 //4- Criar um loop que conte todos os números pares de 0 a 100
+for(let d = 0; d <= 100; d+=2){
+    console.log(d)
+}
